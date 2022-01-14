4788df4f55704e4e681630a11e88d654bdae9e2a
salvadocs <salvag143@gmail.com>
2022-01-14 14:04:36 +0100

    Structure and content of the website finalized.

---
+++ Salvador/html.js
@@ -0,0 +1,23 @@
+/*
+Javascript file for git.html web page.
+*/
+
+function openNav() {
+    /*
+    Function to open side bar if it's close.
+    */
+    document.getElementById("openbtn").style.visibility = "hidden";
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("container").style.marginLeft = "260px";
+    document.getElementById("title").style.marginLeft = "260px";
+}
+
+function closeNav() {
+    /*
+    Function to close side bar if it's open.
+    */
+    document.getElementById("openbtn").style.visibility = "visible";
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("container").style.marginLeft= "10px";
+    document.getElementById("title").style.marginLeft= "10px";
+}
